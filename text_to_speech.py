83e8dd23a78f20575c7b072b280b31e1d1203ee7
Ankit Tyagi <ankittyaagi@gmail.com>
2019-09-12 04:40:00 +0530

    changes

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,10 +2,13 @@ from gtts import gTTS
 
 import os
 
-myText = "text to speech conversion using python and gTTS"
+myText1 = "text to speech conversion using python and gTTS"
+fh = open("sample.text","r")
+myText = fh.read().replace("\n"," ")
 
 language = 'en'
 
 output = gTTS(text=myText,lang = language,slow=False)
 output.save("output.mp3")
+fh.close()
 os.system("start output.mp3")
